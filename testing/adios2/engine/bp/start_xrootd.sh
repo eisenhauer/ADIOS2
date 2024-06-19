3435c7c60f4c559c2cc2f5baec0847670998b0d7
Greg Eisenhauer <eisen@cc.gatech.edu>
2024-06-19 15:25:55 -0400

    again

--- testing/adios2/engine/bp/start_xrootd.sh
+++ testing/adios2/engine/bp/start_xrootd.sh
@@ -3,4 +3,4 @@ if [ "$(id -u)" -eq 0 ]; then
     # we run as root in CI in docker images, this is OK, but we have to tell XRootD that it's OK.
     ROOT_ARGS="-R root"
 fi
-"$1" -b -l /tmp/xroot.log "$ROOT_ARGS" -w "$2" -c $2/xroot/etc/xrootd/xrootd-ssi.cfg
+"$1" -b -l /tmp/xroot.log "$ROOT_ARGS" -w "$2" -c "$2"/xroot/etc/xrootd/xrootd-ssi.cfg
