855841ddd624b5022316c224cc7349cd695f811d
Greg Eisenhauer <eisen@cc.gatech.edu>
2024-06-19 15:22:04 -0400

    kill verbosity

--- testing/adios2/engine/bp/start_xrootd.sh
+++ testing/adios2/engine/bp/start_xrootd.sh
@@ -3,5 +3,4 @@ if [ "$(id -u)" -eq 0 ]; then
     # we run as root in CI in docker images, this is OK, but we have to tell XRootD that it's OK.
     ROOT_ARGS="-R root"
 fi
-echo running $1 in directory $2 with root args $ROOT_ARGS
 "$1" -b -l /tmp/xroot.log "$ROOT_ARGS" -w "$2" -c $2/xroot/etc/xrootd/xrootd-ssi.cfg
